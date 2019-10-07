848fa47c163c148e528eda48c07f9a853d4cac59
Wackery <32279125+webmsgr@users.noreply.github.com>
2019-10-07 13:08:51 -0700

    Update make.py

--- make.py
+++ make.py
@@ -10,10 +10,14 @@ def main():
     except:
         shutil.rmtree("build")
         os.mkdir("build")
+    os.mkdir("build/minorGems/")
     print("Copying minorGems directory struture...")
+    shutil.copytree("./minorGems","./build/minorGems"
 def include(file):
     try:
         cppHeader = CppHeaderParser.CppHeader(file)
     except CppHeaderParser.CppParseError as e:
         print(e)
         sys.exit(1)
+if __name__ = "__main__":
+    main()
