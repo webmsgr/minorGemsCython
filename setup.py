e9e2dcff05cc52525cf98776c05584266c6de422
Wackery <32279125+webmsgr@users.noreply.github.com>
2019-10-07 12:36:06 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from distutils.core import setup
-from distutils.extension import Extension
+from Cython.Build import cythonize
 
 setup(
-    ext_modules = [Extension("minorGems", ["minorGems.pyx"],languge="c++")]
+    name="minorGems",
+    ext_modules=cythonize("minorGems.pyx", languge="c++"),
 )
